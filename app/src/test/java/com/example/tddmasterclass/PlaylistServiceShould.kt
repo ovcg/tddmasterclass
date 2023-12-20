cba033476f84488018659e09d9bd29b1ff9d8073
otavio <17763111+ovcg@users.noreply.github.com>
2023-12-20 16:14:14 -0300

    optimize imports

--- app/src/test/java/com/example/tddmasterclass/PlaylistServiceShould.kt
+++ app/src/test/java/com/example/tddmasterclass/PlaylistServiceShould.kt
@@ -3,14 +3,11 @@ package com.example.tddmasterclass
 import com.example.tddmasterclass.playlist.Playlist
 import com.example.tddmasterclass.utils.BaseUnitTest
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.Mockito.mock
-import org.mockito.Mockito.times
 import org.mockito.Mockito.`when`
-import org.mockito.Mockito.verify
 
 class PlaylistServiceShould : BaseUnitTest() {
 
