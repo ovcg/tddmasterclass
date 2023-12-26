cf555fa3e7152a90804f20e1b455d25a64444f4d
otavio <17763111+ovcg@users.noreply.github.com>
2023-12-26 17:22:27 -0300

    test: playlist details feature complete

--- app/src/androidTest/java/com/example/tddmasterclass/details/PlaylistDetailsFeature.kt
+++ app/src/androidTest/java/com/example/tddmasterclass/details/PlaylistDetailsFeature.kt
@@ -2,14 +2,13 @@ package com.example.tddmasterclass.details
 
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
 import com.example.tddmasterclass.BaseUITest
 import com.example.tddmasterclass.R
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
 import org.hamcrest.CoreMatchers
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -32,8 +31,8 @@ class PlaylistDetailsFeature : BaseUITest() {
             )
         ).perform(ViewActions.click())
 
-        onView(withId(R.id.playlist_name)).check(matches(withText("Hard Rock Cafe")))
+        assertDisplayed("Hard Rock Cafe")
 
-        onView(withId(R.id.playlist_details)).check(matches(withText("Rock your senses with this timeless signature vibe list. \n\n • Poison \n • You shook me all night \n • Zombie \n • Rock'n Me \n • Thunderstruck \n • I Hate Myself for Loving you \n • Crazy \n • Knockin' on Heavens Door")))
+        assertDisplayed("Rock your senses with this timeless signature vibe list. \n\n • Poison \n • You shook me all night \n • Zombie \n • Rock'n Me \n • Thunderstruck \n • I Hate Myself for Loving you \n • Crazy \n • Knockin' on Heavens Door")
     }
 }
